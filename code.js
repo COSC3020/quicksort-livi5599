bb93811b52bd8b579b0099d98095f2c4a2bae309
livi5599 <obrown1@uwyo.edu>
2024-11-03 16:00:45 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,43 @@
 function quicksort(array) {
+    let sortedArr = [];
+    var size = array.length;
+    while (size != 0) {
+        pivot = array[0];
+        for (i = array.length-1; i > array.indexOf(pivot); i--) {
+            if (array[i] < pivot) {
+                [array[array.indexOf(pivot)], array[i]] = [array[i], array[array.indexOf(pivot)]];
+            }
+        }
+        for (i = 0; i < array.indexOf(pivot); i++) {
+            if (array[i] > pivot) {
+                [array[array.indexOf(pivot)], array[i]] = [array[i], array[array.indexOf(pivot)]];
+            }
+        }
+        
+        sortedArr[array.indexOf(pivot)] = pivot;
+        size -= 1;
+    }
+
+    isSorted = false;
+    while (isSorted == false) {
+        if (array.length != sortedArr.length) {
+            newPivot = array[sortedArr.length];
+            for (i = array.length-1; i > array.indexOf(newPivot); i--) {
+                if (array[i] < newPivot) {
+                    [array[array.indexOf(newPivot)], array[i]] = [array[i], array[array.indexOf(newPivot)]];
+                }
+            }
+            for (i = 0; i < array.indexOf(newPivot); i++) {
+                if (array[i] > newPivot) {
+                    [array[array.indexOf(newPivot)], array[i]] = [array[i], array[array.indexOf(newPivot)]];
+                }
+            }
+            sortedArr[array.indexOf(newPivot)] = newPivot;
+            console.log(sortedArr);
+        }
+        else {
+            isSorted = true;
+        }
+    }
     return array;
 }
